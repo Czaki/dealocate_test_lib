b4dae5b3361081f8a381ad234c42dba9a6fd9b8f
Grzegorz Bokota <bokota+github@gmail.com>
2020-01-14 00:11:35 +0100

    configure
    verbose ssh

--- base_compile_libs.sh
+++ base_compile_libs.sh
@@ -3,6 +3,9 @@ if [ "$CXX" = "" ]; then
     CXX=c++
 fi
 
-$CXX_64 -o liba.6.dylib -mmacosx-version-min=10.${NUM} -dynamiclib a.cc
-$CXX_64 -o libb.6.dylib -mmacosx-version-min=10.${NUM} -dynamiclib b.cc -L. -la.${NUM}
-$CXX_64 -o libc.6.dylib -mmacosx-version-min=10.${NUM} -dynamiclib c.cc -L. -la.${NUM} -lb.${NUM}
+CXX_64="$CXX -arch x86_64"
+CXX_32="$CXX -arch i386"
+
+$CXX_64 -o liba.${NUM}.dylib -mmacosx-version-min=10.${NUM} -dynamiclib a.cc
+$CXX_64 -o libb.${NUM}.dylib -mmacosx-version-min=10.${NUM} -dynamiclib b.cc -L. -la.${NUM}
+$CXX_64 -o libc.${NUM}.dylib -mmacosx-version-min=10.${NUM} -dynamiclib c.cc -L. -la.${NUM} -lb.${NUM}
